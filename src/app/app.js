4dfdba6c89e0200e260cb8d88f64ffbd630005bc
Vlad <mortimortall@mail.ru>
2018-09-15 12:50:01 +0400

    create project

---
+++ src/app/app.js
@@ -0,0 +1,3 @@
+import product from './product/product.js';
+
+product();
---
+++ src/app/product/product.js
@@ -0,0 +1,25 @@
+export default function product() {
+    let products = document.querySelectorAll('.product-list__product');
+
+    if ( products.length > 0) {
+        [].forEach.call(products, (product) => {
+            product.addEventListener('mouseenter', () => {
+                productHover(product);
+            });
+            product.addEventListener('mouseleave', () => {
+                productNotHover(product);
+            });
+        });
+    }
+
+    function productHover(product) {
+        let height = product.clientHeight;
+        product.style.height = `${height}px`;
+        product.classList.add('product-list__product--hover')
+    }
+    
+    function productNotHover(product) {
+        product.style.height = '';
+        product.classList.remove('product-list__product--hover')
+    }
+};
